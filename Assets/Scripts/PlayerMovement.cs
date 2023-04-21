9ac3f79332a64e9cbb23c54343c8fc651c4087b3
Gabriel Machado <gabriel.machado.cg@gmail.com>
2023-04-20 23:08:57 -0300

    Player punch feature

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -10,14 +10,16 @@ public class PlayerMovement : MonoBehaviour
         instance = this;
     }
 
-    [SerializeField] private float movementRange;
+    [SerializeField] private float maxSpeed;
     [SerializeField] private float movementSpeed;
 
     [SerializeField] private Animator anim;
 
-    [SerializeField] private Vector3 inputStartPos;
+    private Vector3 inputStartPos;
     private Vector3 currentInput;
     private bool mouseDown;
+
+    private bool isPunching = false;
 
     void Start()
     {
@@ -39,25 +41,49 @@ public class PlayerMovement : MonoBehaviour
             return;
         }
         Vector3 currentMousePos = Input.mousePosition;
-        Vector3 targetInput = (currentMousePos - inputStartPos);
-        if (!mouseDown) targetInput = Vector3.zero;
-        currentInput = Vector3.Lerp(currentInput, targetInput, .2f);
+        Vector3 targetInput = mouseDown ? (currentMousePos - inputStartPos) : targetInput = Vector3.zero;
+        currentInput = Vector3.ClampMagnitude(Vector3.Lerp(currentInput, targetInput, .2f), maxSpeed);
 
         // Calculates next position to target
+        float finalSpeed = isPunching ? movementSpeed * .3f : movementSpeed;
         Vector3 targetPos =
-            new Vector3(transform.position.x + (currentInput.x * movementSpeed * .01f),
+            new Vector3(transform.position.x + (currentInput.x * finalSpeed * .01f),
             0,
-            transform.position.z + (currentInput.y * movementSpeed * .01f));
+            transform.position.z + (currentInput.y * finalSpeed * .01f));
         transform.position = Vector3.Lerp(transform.position, targetPos, .2f);
 
         // Adjust rotation to movement direction
         Quaternion targetRot = Quaternion.LookRotation(targetPos - transform.position);
         targetRot.z = 0;
         targetRot.x = 0;
-        if(currentInput.magnitude > .2f)
-            anim.transform.rotation = Quaternion.Lerp(anim.transform.rotation, targetRot, .3f);
+
+        float finalRotLerp = isPunching ? .05f : .2f;
+        if (currentInput.magnitude > .2f)
+            anim.transform.rotation = Quaternion.Lerp(anim.transform.rotation, targetRot, finalRotLerp);
 
         // Configure animation 
         anim.SetFloat("MovBlend", currentInput.magnitude/100);
+
+        if (Input.GetKeyDown(KeyCode.P)) anim.SetTrigger("PunchTrigger");
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log($"colission with {other.gameObject.name}");
+        if(other.tag == "Enemy")
+            anim.SetTrigger("PunchTrigger");
+
+        isPunching = true;
+        Invoke("SetUnPunch", 1.1f);
+    }
+
+    public void ApplyPunchEffect()
+    {
+        Debug.Log("PUNCH EFFECT");
+    }
+
+    private void SetUnPunch()
+    {
+        isPunching = false;
     }
 }
